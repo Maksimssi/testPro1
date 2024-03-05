a08bc68b5d592a99ef4fa26f0b6aed9df0e24fc7
maksimssi <kbj3461@gmail.com>
2024-03-05 10:40:49 +0900

    aa

--- src/main/java/ch01/Test01.java
+++ src/main/java/ch01/Test01.java
@@ -4,7 +4,7 @@ public class Test01 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("===============1234444444444555555");
+		System.out.println("===============12344444444445555556666");
 
 	}
 
